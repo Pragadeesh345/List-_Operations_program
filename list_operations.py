c31f16ecd505a14d4dc00d548e0b085b50f3e2e6
Pragadeesh A <pragadeesh.cse2022@jerusalemengg.ac.in>
2026-03-28 21:40:53 +0530

    Refactor list operations and improve user input handling and  menu addition
    Refactored functions for clarity and added input handling in main.

--- list_operations.py
+++ list_operations.py
@@ -6,17 +6,21 @@ def sum_list(c):
     return total
 
 def average_list(c):
-    return sum_list(c)/len(c)
+    t=0  # repeating sum of list process so it doesn't crash when choice = 2 without calculating sum of list first
+    for i in c:
+        t+=i
+    average=t/len(c)
+    return average
 
 
-def largest(c):
+def largest_list(c):
     largest=c[0]
     for i in c:
         if i>largest:
             largest=i
     return largest
 
-def smallest(c):
+def smallest_list(c):
     smallest=c[0]
     for i in c:
         if i<smallest:
@@ -63,17 +67,42 @@ def highest_frequency(c):
 
 
 def main():
-    c=[10,20,30,40,10,50,30,30]
+    print("Enter values with spaces for list")
+    print("Example: 1 2 3 4")
+    c=list(map(int,input("Enter the Values for list:").split()))
+    while True:
+            print("----------------------------------")
+            print("1 Sum of List")
+            print("2 Average of List")
+            print("3 Largest of List")
+            print("4 Smallest of List")
+            print("5 Duplicates in List")
+            print("6 Highest Frequency in List")
+            print("0 Exit")
+            print("----------------------------------")
 
-    print("----------------------------------")
-    print(f"Sum of List:",sum_list(c))
-    print(f"Average of List:",average_list(c))
-    print(f"Largest of List:",largest(c))
-    print(f"Smallest of List:",smallest(c))
-    print(f"Duplicates in List:",duplicates(c))
-    print(f"Highest Frequency in List:",highest_frequency(c))
-    print("----------------------------------")
+            x=int(input("Enter your choice:"))
 
-
+            if x == 1:
+                total=sum_list(c)
+                print("Sum of list:",total)
+            elif x == 2:
+                average=average_list(c)
+                print("Average of list:",average)
+            elif x == 3:
+                largest=largest_list(c)
+                print("Largest in List:",largest)
+            elif x == 4:
+                smallest=smallest_list(c)
+                print("Smallest in list:",smallest)
+            elif x == 5:
+                e=duplicates(c)
+                print("Duplicates in List:",e)
+            elif x == 6:
+                element=highest_frequency(c)
+                print("Highest Frequent element in list:",element)
+            elif x == 0:
+                print("Exiting...")
+                break
 
 main()
